026f7c0d69c21955986b85c04f7b88d70392a86b
rezch <a@b>
2025-05-28 21:21:04 +0000

    lab5

---
+++ client_serv_hw/lab5/search.php
@@ -0,0 +1,32 @@
+<?php
+
+// DB vars
+$DBHost = "localhost";
+$DBUser = "phpmyadmin";
+$DBPassword = "***sql123";
+$DBName = "phone_book";
+
+$DBLink = mysqli_connect($DBHost, $DBUser, $DBPassword, $DBName);
+
+$phone = trim($_POST["phone"]);
+
+$Query = "SELECT
+	full_name,
+    address
+FROM `contacts`
+WHERE `phone` = '$phone';";
+
+$Result = mysqli_query($DBLink, $Query);
+
+if ($Result->num_rows == 0) {
+    echo "Ничего не найдено.";
+}
+
+while ($Rows = mysqli_fetch_array($Result, MYSQLI_ASSOC))
+{
+    printf("ФИО:%s, Адрес:%s<br>",
+        $Rows['full_name'], $Rows['address']);
+}
+
+mysqli_close($DBLink);
+?>
